1c04bd39b6501e8f1fc14c9b196cae05370f4732
Edward Lau <eklau@ucsd.edu>
2016-04-26 17:19:37 -0700

    Some front end stuff

---
+++ public/js/weed.js
@@ -0,0 +1,18 @@
+$(function() {
+
+
+  $("#toGraph").on("click", function(event) {
+    event.preventDefault();
+    var href = this.href;
+    $('html, body').animate({
+      scrollTop: $("#team-graph").offset().top
+    }, 1000);
+  });         
+    $("#toAbout").on("click", function(event) {
+    event.preventDefault();
+    var href = this.href;
+    $('html, body').animate({
+      scrollTop: $("#team-about").offset().top
+    }, 1000);
+  });
+});
