4242f29f387d0e9c7bbdb27e50e765a4e4817ca1
Macklin Lee <macklinblee@gmail.com>
2016-04-27 21:05:21 -0700

    locations on the left

--- public/js/index.js
+++ public/js/index.js
@@ -1,84 +1,6 @@
 (function(d3) {
   "use strict";
 
-  var data = [
-    { name: "Lolita's", rating: 7.5 },
-    { name: "Lucha Libre", rating: 8 },
-    { name: "Puesto", rating: 9.5 },
-    { name: "Rubio's", rating: 4 },
-    { name: "Taco Bell", rating: 3 },
-    { name: "Taco Stand", rating: 8.5 },
-    { name: "Taco's, El Gordo", rating: 9 },
-    { name: "Oscar's", rating: 9 },
-    { name: "Rigoberto's", rating: 6 },
-    { name: "Galaxy Taco", rating: 6.5 },
-  ];
-
-  // Defining the margins and chart size
-  // See margin conventions for more information
-  var margin = {top: 20, right: 10, bottom: 100, left: 40},
-      width = 960 - margin.right - margin.left,
-      height = 500 - margin.top - margin.bottom;
-
-  var innerWidth  = width  - margin.left - margin.right;
-  var innerHeight = height - margin.top  - margin.bottom;
-
-  // TODO: Input the proper values for the scales
-  var xScale = d3.scale.ordinal().rangeRoundBands([0, innerWidth], 0.5);
-  var yScale = d3.scale.linear().range([innerHeight, 0]);
-
-  // Define the chart
-  var chart = d3
-                .select(".chart")
-                .append("svg")
-                .attr("width", width + margin.right + margin.left)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" +  margin.left + "," + margin.right + ")");
-
-//var rotateTranslate = d3.svg.transform().rotate(-45).translate(5,0);
-
-  // Render the chart
-  xScale.domain(data.map(function (d){ return d.name; }));
-
-  // TODO: Fix the yScale domain to scale with any ratings range
-  yScale.domain([0, 10]);
-
-  // Note all these values are hard coded numbers
-  // TODO:
-  // 1. Consume the taco data
-  // 2. Update the x, y, width, and height attributes to appropriate reflect this
-  chart
-    .selectAll(".bar")
-    .data(data.map(function (d){ return d.rating; }))
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d, i) { return i*(innerWidth/Object.keys(data).length); })
-    .attr("width", 75)
-    .attr("y", function(d) { return innerHeight-(d*(innerHeight/Object.keys(data).length)); })
-    .attr("height", function(d) { return d*(innerHeight/Object.keys(data).length); });
-
-  // Orient the x and y axis
-  var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-  var yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-  // TODO: Append X axis
-  chart
-    .append("g")
-    .call(xAxis)
-    .attr("transform", "translate( 0," + innerHeight + ")")
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.9em")
-      .attr("dy", ".5em")
-      .attr("transform", "rotate(-45)" );;
-
-
-  // TODO: Append Y axis
-  chart
-    .append("g")
-    .call(yAxis);
-
   // ASSIGNMENT PART 1B
   // Grab the delphi data from the server
   d3.json("/delphidata", function(err, data) {
@@ -91,61 +13,7 @@
 });
 
 })(d3);
-/*
-function delphiChart(data){
-  var margin = {top: 20, right: 10, bottom: 100, left: 50},
-      width = 960 - margin.right - margin.left,
-      height = 500 - margin.top - margin.bottom;
 
-  var innerWidth  = width  - margin.left - margin.right;
-  var innerHeight = height - margin.top  - margin.bottom;
-
-  // TODO: Input the proper values for the scales
-  var xScale = d3.scale.ordinal().rangeRoundBands([0, innerWidth], 0.1);
-  var yScale = d3.scale.linear().range([0, innerHeight]);
-
-  var graph = d3
-                .select(".graph")
-                .append("svg")
-                .attr("width", width + margin.right + margin.left)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" +  margin.left + "," + margin.right + ")");
-
-    xScale.domain(data.map(function (d){ return d.gender; }));
-    yScale.domain([9000, 0]);
-
-    var range = d3.max( data.map(function(d){ return d.number_of_respondents; }) );
-
-    graph
-    .selectAll(".bar")
-    .data(data.map(function (d){ return d.number_of_respondents; }))
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d, i) { return i*(innerWidth/Object.keys(data).length); })
-    .attr("width", 250)
-    .attr("y", function(d) { return innerHeight-(innerHeight*(d/range)); })
-    .attr("height", function(d) { return innerHeight*(d/range); });
-
-    // Orient the x and y axis
-    var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-    var yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-    graph
-    .append("g")
-    .call(xAxis)
-    .attr("transform", "translate( 0," + innerHeight + ")")
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.9em")
-      .attr("dy", ".5em")
-      .attr("transform", "rotate(-45)" );;
-
-    graph
-    .append("g")
-    .call(yAxis);
-}
-*/
 function drugChart(data){
 
   var zipCode = data.map(function (d){ return d.zip; });
@@ -166,20 +34,49 @@ function drugChart(data){
     i++;
   });
   
-$('.precinct').tipsy({
-    gravity: $.fn.tipsy.autoNS,
-    title: function() {
-        //alert("here");
-        return 'Hi there!';
-    }
-});
 
   var infoBox = d3.select("div#info");
   d3.selectAll("g.region")
   .on("mouseover", function(d,i) {
-         infoBox.html("<span style=\"font-size: 50px\">" + count[i] + "</span><br><br><br>ZipCode<br><br><span style=\"font-size: 50px\">" + zipCode[i] + "</span>"); //print the associated data
+		var location = "temp";
+		if (zipCode[i] == 92037) location = "La Jolla";
+		else if(zipCode[i] == 92101) location = "Downtown";
+		else if(zipCode[i] == 92102) location = "South Park";
+		else if(zipCode[i] == 92103) location = "Hillcrest";
+		else if(zipCode[i] == 92104) location = "North Park";
+		else if(zipCode[i] == 92105) location = "City Heights";
+		else if(zipCode[i] == 92106) location = "Point Loma";
+		else if(zipCode[i] == 92107) location = "Ocean Beach";
+		else if(zipCode[i] == 92108) location = "Mission Valley";
+		else if(zipCode[i] == 92109) location = "Pacific Beach";
+		else if(zipCode[i] == 92110) location = "Old Town";
+		else if(zipCode[i] == 92111) location = "Kearny Mesa";
+		else if(zipCode[i] == 92113) location = "Logan Heights";
+		else if(zipCode[i] == 92114) location = "Encanto";
+		else if(zipCode[i] == 92115) location = "San Diego State";
+		else if(zipCode[i] == 92116) location = "University Heights";
+		else if(zipCode[i] == 92117) location = "North Clairemont";
+		else if(zipCode[i] == 92119) location = "Mission Trails";
+		else if(zipCode[i] == 92120) location = "Allied Gardens";
+		else if(zipCode[i] == 92121) location = "Sorrento Valley";
+		else if(zipCode[i] == 92122) location = "University City";
+		else if(zipCode[i] == 92123) location = "Serra Mesa";
+		else if(zipCode[i] == 92124) location = "Tierra Santa";
+		else if(zipCode[i] == 92126) location = "Mira Mesa";
+		else if(zipCode[i] == 92130) location = "Carmel Valley";
+		else if(zipCode[i] == 92131) location = "Scripps Ranch";
+		else if(zipCode[i] == 92139) location = "Paradise Hills";
+		else if(zipCode[i] == 92140) location = "Marine Corps";
+		else{
+			location = "Not Available";
+			}
+         infoBox.html("Location <span style=\"font-size: 50px\"> <br>"+ location+
+		 "</span> <br><br><br>Number of Weed Arrests<br><br><br><span style=\"font-size: 50px\">" + count[i] +
+		 "</span><br><br><br>ZipCode<br><br><span style=\"font-size: 50px\">" +zipCode[i] + "</span>"); //print the associated data
   });
 
+ 
+  
   
 $('#laJolla').tipsy({
     gravity: $.fn.tipsy.autoNS,
