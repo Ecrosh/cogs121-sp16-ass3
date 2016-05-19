64257a65c8c2526c9de04f357e7fb96b2ef02585
Edward Lau <eklau@ucsd.edu>
2016-05-19 02:30:12 -0700

    Skeleton Code Updated

--- public/js/alcojolla.js
+++ public/js/alcojolla.js
@@ -1,4 +1,4 @@
-$(function() {
+(function($) {
   $("#toHome").on("click", function(event) {
     event.preventDefault();
     var href = this.href;
@@ -6,25 +6,42 @@ $(function() {
       scrollTop: $("#team-home").offset().top
     }, 1200);
   });
-  $("#toGraph").on("click", function(event) {
-    event.preventDefault();
-    var href = this.href;
-    $('html, body').animate({
-      scrollTop: $("#team-graph").offset().top
-    }, 1200);
-  });         
   $("#toAbout").on("click", function(event) {
     event.preventDefault();
     var href = this.href;
     $('html, body').animate({
-      scrollTop: ($("#team-about").offset().top - 53)
+      scrollTop: ($("#team-about").offset().top - 56)
+    }, 1200);
+  });         
+  $("#toDanger").on("click", function(event) {
+    event.preventDefault();
+    var href = this.href;
+    $('html, body').animate({
+      scrollTop: ($("#team-danger").offset().top - 56)
     }, 1200);
   });
-});
+  $("#toRisk").on("click", function(event) {
+    event.preventDefault();
+    var href = this.href;
+    $('html, body').animate({
+      scrollTop: ($("#team-risk").offset().top - 56)
+    }, 1200);
+  });
+  $("#toMod").on("click", function(event) {
+    event.preventDefault();
+    var href = this.href;
+    $('html, body').animate({
+      scrollTop: $("#team-mod").offset().top
+    }, 1200);
+  });
+
+
+
+}($));
 
 
 	 
-$(function() {
+(function($) {
   var navOffset = $("#navBar").offset().top;
 
   $(window).scroll(function(){
@@ -40,9 +57,4 @@ $(function() {
       $("#navBar").css("bottom", "0");
     }
   });
-});
-
-function siren() {
-  var audio = new Audio('/audio/police1.wav');
-  audio.play();
-}
+}($));
